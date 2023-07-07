d24a86c310b9a63116acb2aebfdd01d1fdda3e31
Gvidas Kalvonis <gvidaskalv@gmail.com>
2023-07-07 18:16:09 +0100

    Basic Structure made

---
+++ Assets/Scripts/Point.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Point 
+{
+    public int x;
+    public int y;
+    public int pointNumber;
+
+    public Point(int x, int y, int pointNumber)
+    {
+        this.x = x;
+        this.y = y;
+        this.pointNumber = pointNumber;
+    }
+}
